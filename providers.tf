d01b59cd37ab4a8ad56fea05454874ae52ff66dc
testaccountbk <boonkhim.iguides@gmail.com>
2021-04-01 22:19:04 +0800

    corrected providers.tf

--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 terraform {
     backend "s3" {
-        bucket = "javahome-tf-1212"
+        bucket = "javahome-tf-1212-bk"
         key = "terraform.tfstate"
         region = "us-east-1"
     }
