1c1cbd89f718a58012e063884e1dd7e6fc623539
testaccountbk <boonkhim.iguides@gmail.com>
2021-04-01 22:49:03 +0800

    please work la

--- providers.tf
+++ providers.tf
@@ -2,10 +2,11 @@
 provider "aws" {
     region = "${var.region}"
 }
-
 terraform {
     backend "s3" {
-        bucket = "javahome-tf-1212-bk"
+        bucket = "javahome-tf-1212-raybk12"
+        key = "terraform.tfstate"
         region = "us-east-1"
     }
 }
+
