9801492c7cb6e9d01505b27ead2859f3e65a402f
testaccountbk <boonkhim.iguides@gmail.com>
2021-04-01 19:53:40 +0800

    updated jenkinsfile

---
+++ providers.tf
@@ -0,0 +1,11 @@
+provider {
+    region = "${var.region}"
+}
+
+terraform {
+    backend "s3" {
+        bucket = "javahome-tf-1212"
+        key = "terraform.tfstate"
+        region = "us-east-1"
+    }
+}
